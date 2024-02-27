916a58a353b6694f8b8d01f45e6cb11143ce36e9
khushdil <khushdilikhtiyori@gmail.com>
2024-02-27 18:49:26 +0500

    Solving problems in js

---
+++ index.js
@@ -0,0 +1,65 @@
+//1.
+// let i = 1
+// do {
+//     console.log(i)
+//     i++
+// }
+// while(i<=100)
+
+//2.
+// let i = -100
+// while(i<=0){
+//     console.log(i)
+//     i++;
+// }
+
+//3.
+// for (let i = 100; i>=1;i--){
+//     console.log(i)
+// }
+
+//4.
+// for (let i = 2; i<=100;i+=2){
+//         console.log(i)
+// }
+
+//5.
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0) {
+//         console.log(i);
+//     }
+// }
+
+//6.
+// let array = [];
+// for (let i = 1; i <= 10; i++) {
+//     array.push(i);
+// }
+// console.log(array);
+
+//7.
+// let arr = [];
+// for (let i = 2; i <= 100; i += 2) {
+//     arr.push(i);
+// }
+//console.log(arr)
+
+//8.
+// let arr = [2,-3,4,5-7,1];
+// let count = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < 0) {
+//         count++;
+//     }
+// }
+// console.log(count)
+
+// 9.
+// let arr = [2,-2,4,-3,5,-6];
+// let positiveArr = arr.filter(num => num > 0);
+// console.log(positiveArr)
+
+// 10.
+// let str = "ваша строка";
+// str = str.slice(0, -2) + str.slice(-1);
+// console.log(str)
